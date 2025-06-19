000f88ef3b7c43df94ff20163d0cebdc28746498
endabelyu <endabelyuproject@gmail.com>
2025-06-19 14:08:22 +0700

    feat: -add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -19,6 +20,7 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    netlifyPlugin(),
   ],
   resolve: {},
 });
